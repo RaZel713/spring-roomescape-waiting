e8bf91002287726535431c9a31a1a29c19a4cff7
RaZel713 <razel7130@gmail.com>
2025-05-21 17:51:31 +0900

    refactor: '@NonNull' 어노테이션 추가

--- src/main/java/roomescape/presentation/auth/CheckAdminInterceptor.java
+++ src/main/java/roomescape/presentation/auth/CheckAdminInterceptor.java
@@ -2,6 +2,7 @@ package roomescape.presentation.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.domain.auth.AuthenticationTokenHandler;
 
@@ -14,7 +15,9 @@ public class CheckAdminInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
+    public boolean preHandle(@NonNull final HttpServletRequest request,
+                             @NonNull final HttpServletResponse response,
+                             @NonNull final Object handler) {
         if (isCurrentRequestorAdmin(request)) {
             return true;
         }
